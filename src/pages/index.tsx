0fbf263f45c2d4ab8408f1cfde22585207e85b75
LucasFritschy <lucasfritschy@gmail.com>
2021-06-08 15:53:26 -0300

    fetch next page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,16 +32,10 @@ export default function Home(): JSX.Element {
     return data;
   };
 
-  const {
-    data,
-    isLoading,
-    isError,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-  } = useInfiniteQuery('images', fetchImages, {
-    getNextPageParam: ({ after }) => after || null,
-  });
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } =
+    useInfiniteQuery('images', fetchImages, {
+      getNextPageParam: ({ after }) => after || null,
+    });
 
   const formattedData = useMemo(() => {
     return data?.pages.map(imageData => imageData.data).flat();
@@ -61,7 +55,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button my={10} onClick={() => fetchNextPage()}>
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
